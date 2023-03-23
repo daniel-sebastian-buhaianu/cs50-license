10db8d5c42b3973b9ca5462f2010365dd31214ed
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-03-23 06:58:10 +0200

    fixed bug, LGTM now

--- license.c
+++ license.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_PLATES 8
+#define MAX_PLATE_LEN 6
 
 int main(int argc, char *argv[])
 {
@@ -10,26 +14,26 @@ int main(int argc, char *argv[])
     }
 
     // Create buffer to read into
-    char buffer[7];
+    char buffer[MAX_PLATE_LEN + 1];
 
     // Create array to store plate numbers
-    char *plates[8];
+    char plates[MAX_PLATES + 1][MAX_PLATE_LEN + 1];
 
     FILE *infile = fopen(argv[1], "r");
 
     int idx = 0;
 
-    while (fread(buffer, 1, 7, infile) == 7)
+    while (fread(buffer, 1, MAX_PLATE_LEN + 1, infile) == MAX_PLATE_LEN + 1)
     {
         // Replace '\n' with '\0'
-        buffer[6] = '\0';
+        buffer[MAX_PLATE_LEN] = '\0';
 
         // Save plate number in array
-        plates[idx] = buffer;
+	strcpy(plates[idx], buffer);
         idx++;
     }
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < idx; i++)
     {
         printf("%s\n", plates[i]);
     }
